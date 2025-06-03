315b78196ff40d59cb0a3e455c0b3051948f9774
Erdem Boran Bad <erdem_boran_badd@hotmail.com>
2025-06-03 14:37:36 +0300

    Refactored peakCoefficient generation

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -234,17 +234,23 @@ ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
     return settings;
 }
 
-void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings) {
-    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(getSampleRate(), chainSettings.peakFreq, chainSettings.peakQuality,
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate) {
+    return juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate, chainSettings.peakFreq, chainSettings.peakQuality,
         juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
 
+    
+}
+
+void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings) {
+    
+    auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
     
     updateCoefficients(leftChannel.get<ChainPositions::Peak>().coefficients, peakCoefficients);
     updateCoefficients(rightChannel.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 
 }
 
-void SimpleEQAudioProcessor::updateCoefficients(Coefficients &old, const Coefficients &replacements)
+void  updateCoefficients(Coefficients& old, const Coefficients& replacements)
 {
     *old = *replacements;
 }
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -43,6 +43,12 @@ enum ChainPositions
     HighCut
 };
 
+using Coefficients = Filter::CoefficientsPtr;
+
+void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
+
 //==============================================================================
 /**
 */
@@ -97,9 +103,7 @@ private:
        
     void updatePeakFilter(const ChainSettings& chainSettings);
 
-    using Coefficients = Filter::CoefficientsPtr;
-
-    static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+    
 
 
     template<int Index ,typename ChainType, typename CoefficientType>
