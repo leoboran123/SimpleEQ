f11fed6782b354972b22a731eafae919329b9eed
Erdem Boran Bad <erdem_boran_badd@hotmail.com>
2025-06-11 15:26:40 +0300

    Sliders are now circles

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -50,7 +50,6 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
     g.fillPath(p);
 
 
-    //CHAPTER 12 -> CUSTOMIZE SLIDER VALUES (2:31:30)
 
 }
 
@@ -66,6 +65,13 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
     auto range = getRange();
 
     auto sliderBounds = getSliderBounds();
+
+    g.setColour(Colours::red);
+    g.drawRect(getLocalBounds());
+    g.setColour(Colours::yellow);
+    g.drawRect(sliderBounds);
+
+
 
     getLookAndFeel().drawRotarySlider(g,
         sliderBounds.getX(),
@@ -85,7 +91,22 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
 {
-    return getLocalBounds();
+    //return getLocalBounds();
+
+    auto bounds = getLocalBounds();
+
+    auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
+     
+    size -= getTextHeight() * 2;
+
+    juce::Rectangle<int> r;
+    r.setSize(size, size);
+
+    r.setCentre(bounds.getCentreX(), 0);
+    r.setY(2);
+
+    return r;
+    
 }
 
 
