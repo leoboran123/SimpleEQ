eea5082031756b89bb6ea12b90071d2aa23124bf
Erdem Boran Bad <erdem_boran_badd@hotmail.com>
2025-06-26 17:06:27 +0300

    FFT path positioning fixed.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -413,10 +413,11 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
         responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
     }
 
-    g.setColour(Colours::blue);
+    leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
+
+
+    g.setColour(Colours::skyblue);
     g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
-
-
 
     g.setColour(Colours::orange);
     g.drawRoundedRectangle(getRenderArea().toFloat(), 4.f, 1.f);
